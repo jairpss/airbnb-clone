84ddcdec2a59fb40209d4d264d3c3c2ea8225396
Jair Partida <jairpartida1@gmail.com>
2020-12-10 13:11:22 -0700

    Footer done, starting with react router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import Home from './Home';
 import Header from './Header';
+import Footer from './Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Router>
 
-
+        <Header />
+        <Home />
+        <Footer />
+        
+      </Router>
     </div>
   );
 }
