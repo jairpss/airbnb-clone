91345ee00f664b752cb9ed577b7edbda81d6803b
Jair Partida <jairpartida1@gmail.com>
2020-12-09 23:30:24 -0700

    Date picker/Search and Cards are DONE, pending to fix search btn position

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Banner.css';
 import { Button } from '@material-ui/core';
+import Search from './Search';
 
 function Banner() {
     const [showSearch, setShowSearch] = useState (false);
@@ -10,8 +11,11 @@ function Banner() {
             <div className='banner__search'>
                 {showSearch && <Search />}
 
-                <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>
-                    Search Dates
+                <Button onClick={() => 
+                    setShowSearch(!showSearch)} 
+                    className='banner__searchButton' 
+                    variant='outlined'>
+                    {showSearch ? 'Hide' : 'Search Dates'}
                 </Button>
             </div>
             <div className='banner__info'>
---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import { PermDeviceInformationRounded } from '@material-ui/icons';
+import React from 'react';
+import './Card.css';
+
+function Card({src, title, description, price}) {
+    return (
+        <div className='card'>
+            <img src={src} alt='' />
+            <div className='card__info'>
+                <h2>{title}</h2>
+                <h4>{description}</h4>
+                <h3>{price}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
