49f1259367f66613caf28b0361e7729002ba2af5
Jair Partida <jairpartida1@gmail.com>
2020-12-13 18:32:38 -0700

    Deploying the project with firebase, live demo is on the web, all project it's DONE

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 This is an Airbnb clone based on Clever Programming tutorial
 
+Here is the live demo: https://airbnb-clone-ad78c.web.app/
+
 _Stack:_
 
     -React JS
     -Material UI
     -React Hooks
     -React Router
+    -React Date Picker
     -Firebase Hosting
     -Flexbox
-    -Date Picker
--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,9 @@ import Card from './Card';
 function Home() {
     return (
         <div className='home'>
-            {/* <h1>Home component</h1> */}
             <Banner/>
+
+            <h2>Experiences</h2>
             <div className='home__section'>
                 <Card 
                     src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
@@ -25,24 +26,25 @@ function Home() {
                     description="Spaces that are more than just a place to sleep."
                 />
             </div>
+            <h2>Hosting</h2>
             <div className='home__section'>
                 <Card 
                     src="https://media.nomadicmatt.com/2018/apartment.jpg"
                     title="1 Bedroom apartment"
                     description="Superhost with great amenities and a fabolous shopping complex nearby"
-                    price="£70/night"
+                    price="$70/night"
                 />
                 <Card 
                     src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
                     title="3 Bedroom Flat in Bournemouth"
                     description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
-                    price="£130/night"
+                    price="$130/night"
                 />
                 <Card 
                     src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
                     title="Penthouse in London"
                     description="Enjoy the amazing sights of London with this stunning penthouse"
-                    price="£350/night"
+                    price="$350/night"
                 />
             </div>
         </div>
